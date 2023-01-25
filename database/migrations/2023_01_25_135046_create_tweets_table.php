9a5af7ce64a117b9321c623513ca50d92f8236b3
k11n14 <k11n14@icloud.com>
2023-01-25 22:56:01 +0900

    ./vendor/bin/sail php artisan migrate

---
+++ database/migrations/2023_01_25_135046_create_tweets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            // 🔽 ここから追加
+            $table->string('tweet');
+            $table->text('description')->nullable();
+            // 🔼 ここまで追加
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+};
